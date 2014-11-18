1532e2760cb59d02fa4f08b8b470e08f4459b4d4
mauro verzetti <mverzett@cern.ch>
2014-11-18 14:24:33 +0100

    first commit of framework setup

---
+++ environment.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+
+# Setup the environment for the framework
+
+echo "Setting up CMSSW runtime environment"
+eval `scramv1 runtime -sh`
+
+export fwk=$CMSSW_BASE/src/placeholder/
+echo "Setting variable \$fwk=$fwk"
+
+export base=$CMSSW_BASE/src
+
+vpython=$base/placeholder/external/virtualenv
+echo "Activating python virtualenv from $vpython"
+
+# Define some shortcuts to HDFS and scratch areas
+export hdfs=/hdfs/store/user/$LOGNAME/
+export scratch=/scratch/$LOGNAME/
+
+if [ -d "$vpython" ]; then
+  echo "Activating python virtual environment"
+  export VIRTUAL_ENV_DISABLE_PROMPT=1
+  pushd $vpython
+  # See https://github.com/pypa/virtualenv/issues/150
+  source bin/activate
+  popd
+fi
+
+# Put the PWD into the PYTHONPATH
+export PYTHONPATH=.:$PYTHONPATH
+# Make sure we prefer our virtualenv packages
+export PYTHONPATH=$vpython/lib/python2.7/site-packages/:$PYTHONPATH
+
+# Don't require a scram build to get updated scripts
+export PATH=$fwk/Utilities/scripts:$PATH
