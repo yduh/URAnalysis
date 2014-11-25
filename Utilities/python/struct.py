bafc41d6adc23d61a3836e71e1e961faa5266dae
mauro verzetti <mverzett@cern.ch>
2014-11-25 16:20:34 +0100

    other two useful modules

---
+++ Utilities/python/struct.py
@@ -0,0 +1,25 @@
+'''
+python dict - to class
+copyed from: http://stackoverflow.com/questions/1305532/convert-python-dict-to-object 
+Author: Mauro Verzetti
+'''
+from copy import deepcopy
+
+class struct:
+    def __init__(self, **entries): 
+        self.__dict__.update(entries)
+
+    def clone(self, **subs):
+        newd = deepcopy(self.__dict__)
+        newd.update(subs)
+        return struct(**newd)
+
+class RecursiveStruct:
+    def __init__(self, **entries):
+        class_dict = {}
+        for key, value in entries.iteritems():
+            if isinstance(value, dict):
+                class_dict[key] = RecursiveStruct(**value)
+            else:
+                class_dict[key] = value
+        self.__dict__.update(class_dict)
