675402c40f3082d35977cd836ceffb57baa6ad46
Mauro Verzetti <mverzett@cern.ch>
2015-03-17 03:36:24 -0500

    Simple (maybe too simple) script that holds the process until all condor jobs are done

---
+++ AnalysisTools/scripts/hold.py
@@ -0,0 +1,21 @@
+#! /bin/env python
+
+'''
+holds the process until all condor jobs are done
+'''
+
+import subprocess, time, os
+from pdb import set_trace
+
+escape = False
+while not escape:
+   proc = subprocess.Popen(["condor_q", os.environ['USER']], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+   stdout, stderr = proc.communicate()
+   last = stdout.split('\n')[-2]
+   njobs = int(last.split()[0])
+   if njobs == 0:
+      print "Jobs completed! exiting..."
+      escape = True
+   else:
+      print "%i jobs are still running, checking again in 30 seconds"
+      time.sleep( 30 )
