405b81a538f6bd030b9f9c59ef3a4c48f11220a3
Mauro Verzetti <mverzett@cern.ch>
2015-03-20 02:54:08 -0500

    printout and check exit codes of the jobs, chrash if a job failed in the process. More strict directory listing to skip spurious files

--- AnalysisTools/scripts/addjobs.py
+++ AnalysisTools/scripts/addjobs.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python2
 #run this script in the job collection directory to add all output files in the subdirectories
 import sys, os, time
+from glob import glob
+import re
 
 allfiles = os.listdir('.')
 
 jobdirs = [d for d in allfiles if os.path.isdir(d)]
+regex = re.compile('exit code: (?P<exitcode>\d+)')
 
 for dir in jobdirs:
   print dir
@@ -16,7 +19,21 @@ for dir in jobdirs:
   info = [l.strip() for l in info if 'Queue' in l]
   num =  int(info[0].split('Queue')[1].strip())
   files = os.listdir(dir)
+  stdouts = [i for i in files if '.stdout' in i]
   files = [f for f in files if ('%s_out_' % dir) in f]
+
+  #check for errors in job processing
+  for stdout in stdouts:
+    last_line = open(os.path.join(dir, stdout)).readlines()[-1]
+    match = regex.match(last_line)
+    if match:
+      exitcode = int(match.group('exitcode'))
+      if exitcode != 0 :
+        raise IOError('Condor job %s did not complete properly and exited'
+                      ' returing status %i' % (stdout, exitcode))
+    else:
+      raise ValueError("cannot match %s with exit code regex!" % last_line)
+
   print num, len(files)
   if num == len(files):
     files = [dir + '/' + f for f in files]
